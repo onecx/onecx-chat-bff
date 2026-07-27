b6af30fcac570a0659bb39289ffc2a564982716e
fadeleus <fadel.kaadan@gmail.com>
2026-07-27 10:05:49 +0200

    feat: add server exceptiopn mappers

--- src/main/java/org/tkit/onecx/chat/bff/rs/controllers/AgentRestController.java
+++ src/main/java/org/tkit/onecx/chat/bff/rs/controllers/AgentRestController.java
@@ -1,19 +1,25 @@
 package org.tkit.onecx.chat.bff.rs.controllers;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.ClientWebApplicationException;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 import org.openapi.quarkus.onecx.ai.provider.svc.v1.client.api.AgentApi;
 import org.openapi.quarkus.onecx.ai.provider.svc.v1.client.model.AgentPageResult;
 import org.openapi.quarkus.onecx.ai.provider.svc.v1.client.model.AgentSearchCriteria;
 import org.tkit.onecx.chat.bff.rs.mappers.AgentMapper;
+import org.tkit.onecx.chat.bff.rs.mappers.ExceptionMapper;
 
 import gen.org.tkit.onecx.chat.bff.rs.internal.AgentApiService;
 import gen.org.tkit.onecx.chat.bff.rs.internal.model.AgentSearchCriteriaDTO;
+import gen.org.tkit.onecx.chat.bff.rs.internal.model.ProblemDetailResponseDTO;
 
+@ApplicationScoped
 public class AgentRestController implements AgentApiService {
 
     @Inject
@@ -23,8 +29,11 @@ public class AgentRestController implements AgentApiService {
     @Inject
     AgentMapper mapper;
 
+    @Inject
+    ExceptionMapper exceptionMapper;
+
     @Override
-    public Response findAgentBySearchCriteria(@Valid @NotNull AgentSearchCriteriaDTO agentSearchCriteriaDTO) {
+    public Response findAgentBySearchCriteria(AgentSearchCriteriaDTO agentSearchCriteriaDTO) {
         AgentSearchCriteria agentSearchCriteria = mapper.mapCriteria(agentSearchCriteriaDTO);
 
         try (Response searchResponse = client.findAgentBySearchCriteria(agentSearchCriteria)) {
@@ -33,4 +42,13 @@ public class AgentRestController implements AgentApiService {
         }
     }
 
+    @ServerExceptionMapper
+    public Response exception(ClientWebApplicationException ex) {
+        return exceptionMapper.clientException(ex);
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ProblemDetailResponseDTO> constraint(ConstraintViolationException ex) {
+        return exceptionMapper.constraint(ex);
+    }
 }
